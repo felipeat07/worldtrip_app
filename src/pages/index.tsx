c01f1ea24aeed83703c81852d800836b078d794d
Felipe Tavares <felipeat07@gmail.com>
2022-07-17 21:49:46 +0100

    styling index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import * as React from 'react'
-import { Box, ChakraProvider, HStack, Image, Stack, Text } from '@chakra-ui/react'
+import { Box, ChakraProvider, Divider, HStack, Image, Stack, Text } from '@chakra-ui/react'
 import { Header } from '../components/Header'
 
 
@@ -9,43 +9,97 @@ const Home: NextPage = () => {
     <ChakraProvider>
       <Header />
 
-      <Box
-        maxWidth='100%'
-        h="335"
-        backgroundImage="Background.svg"
-      >
-        <HStack
-          spacing={200}
-          align='flex-start'
-          paddingTop="100"
-          paddingLeft="140"
+      <Stack spacing={20}>
+
+        <Box
+          maxWidth='100%'
+          h="335"
+          backgroundImage="Background.svg"
         >
-          <Box>
-            <Stack
-              spacing={4}
-              w='100%'
-            >
-              <Text fontSize='4xl' color='white' >
-                5 Continentes, <br /> infinitas possibilidades.
-              </Text>
+          <HStack
+            spacing={200}
+            align='flex-start'
+            paddingTop="100"
+            paddingLeft="140"
+          >
+            <Box>
+              <Stack
+                spacing={3}
+                w='100%'
+              >
+                <Text fontFamily='Poppins' fontWeight='medium' fontSize='4xl' color='white' >
+                  5 Continentes, <br /> infinitas possibilidades.
+                </Text>
 
-              <Text fontWeight='hairline' fontSize='xl' color='white'>
-                Chegou a hora de tirar do papel a viagem que você <br /> sempre sonhou.
-              </Text>
-            </Stack>
+                <Text fontFamily='Poppins' fontWeight='regular' fontSize='xl' color='white'>
+                  Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+                </Text>
+              </Stack>
 
-          </Box>
-          <Box>
-            <Image
-              src='Airplane.svg'
-            />
+            </Box>
 
-          </Box>
-        </HStack>
+            <Box>
+              <Image
+                src='Airplane.svg'
+              />
 
-      </Box>
+            </Box>
+          </HStack>
 
+        </Box>
 
+        <Box>
+
+          <HStack
+            spacing={20}
+            marginTop={20}
+            justifyContent='center'
+          >
+
+            <Box>
+              <Image src='Nightlife.svg' />
+            </Box>
+
+            <Box>
+              <Image src='Beach.svg' />
+            </Box>
+
+            <Box>
+              <Image src='Modern.svg' />
+            </Box>
+
+            <Box>
+              <Image src='Classic.svg' />
+            </Box>
+
+            <Box>
+              <Image src='More.svg' />
+            </Box>
+
+          </HStack>
+
+        </Box>
+
+        <Box 
+          borderTop='2px solid #47585B' 
+          width={90} 
+          alignSelf='center'
+        >
+        </Box>
+
+        <Box>
+          <Text 
+            fontFamily='Poppins' 
+            fontWeight='medium' 
+            fontSize='4xl' 
+            color='#47585B' 
+            align='center'
+          >
+                  Vamos nessa? <br /> Então escolha seu continente
+          </Text>
+        </Box>
+
+      </Stack>
 
     </ChakraProvider>
   )
